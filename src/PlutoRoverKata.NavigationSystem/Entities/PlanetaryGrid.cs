3e7a1e69818d618150409cd1185bbde67154b039
CromixPT <tiago.g.candeias@gmail.com>
2022-12-11 22:01:02 +0000

    Add PlanetaryGrid

---
+++ src/PlutoRoverKata.NavigationSystem/Entities/PlanetaryGrid.cs
@@ -0,0 +1,21 @@
+﻿namespace PlutoRoverKata.NavigationSystem.Entities;
+public sealed class PlanetaryGrid
+{
+    public int Height { get; init; } = 100;
+    public int Width { get; init; } = 100;
+
+    public PlanetaryGrid(int height, int width)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    public bool isPositionWithinBounds(Position position)
+    {
+        if(position.XCoordinate <= 0 || position.YCoordinate <= 0)
+        {
+            return false;
+        }
+        return position.XCoordinate <= Height && position.YCoordinate <= Width;
+    }
+}
